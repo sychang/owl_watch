4330ac029833c593e371742abcc48821d8407df4
Allison Yee <alcyee@berkeley.edu>
2016-04-29 15:42:32 -0700

    combined sonar light

--- sonar light.py
+++ sonar light.py
@@ -3,17 +3,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 import pygame
 
-import pygame
-
 #PLAY AUDIO CLIP
 def play_audio(clip_to_play):
-    print 'play audio start'
-    pygame.mixer.init()
-    pygame.mixer.music.load(clip_to_play)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-      continue
-
+  print 'play audio start'
+  pygame.mixer.init()
+  pygame.mixer.music.load(clip_to_play)
+  pygame.mixer.music.play()
+  while pygame.mixer.music.get_busy() == True:
+    continue
 
 TRIG = 20 
 ECHO = 26
@@ -21,6 +18,11 @@ ECHO = 26
 red = 17
 green = 27
 blue = 18
+
+# Buttons TODO: UPDATE
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print "Distance Measurement In Progress"
 
@@ -47,6 +49,11 @@ while(True):
 	time.sleep(0.00001)
 	GPIO.output(TRIG, False)
 
+	# UPDATE THESE PINS
+	panic_button = GPIO.input(11)
+	off_button = GPIO.input(11)
+	report_button = GPIO.input(11)
+
 	while GPIO.input(ECHO)==0:
 		pulse_start = time.time()
 
@@ -61,14 +68,26 @@ while(True):
 	GPIO.output(red, 0)
 	GPIO.output(green, 0)
 	GPIO.output(blue, 0)
-	#if button pressed:
-		#send SOS 
+
+	if panic_button == False:
+    print('Panic Button Pressed')
+    play_audio('panic.mp3')
+    time.sleep(0.2)
+  elif off_button == False:
+  	print('Off Button Pressed')
+    time.sleep(0.2)
+  elif report_button == False:
+  	print('Report Button Pressed')
+  	play_audio('report.mp3')
+  	time.sleep(0.2)
+	
 	#if distance < 10:
 		#not manually shut off within x time, send alert
 		#speaker gets really loud
 	if distance < 50:
 		#activates timed warning
 		GPIO.output(red, 1)
+		
 	elif distance < 300:
 		GPIO.output(blue, 1)
 		
